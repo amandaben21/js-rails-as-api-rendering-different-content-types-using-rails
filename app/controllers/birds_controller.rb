937f0c65bea22aeafd6531147f3e0d9de589096c
Amanda <amandaben21@aol.com>
2021-05-18 22:58:21 -0400

    Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,11 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    #@birds = Bird.all
+    #render plain: "Hello #{@birds[3].name}"
+    #render json: @birds
+    #render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
+    
+    render json: birds #Don't neeed instant variables anymore
   end
 end
